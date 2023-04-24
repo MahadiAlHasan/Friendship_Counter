4275c90249d3ae7903c82f5809993cff2690328e
Mubashwir Mahadi <131578101+MubashwirMahadi@users.noreply.github.com>
2023-04-24 11:11:00 +0600

    Update and rename .gitignore to Friendship_Counter.py

---
+++ Friendship_Counter.py
@@ -0,0 +1,14 @@
+#Importing Random
+import random
+
+#Taking User's Friend Name
+x1 = input("First Friend's Name :")
+x2 = input("Second Friend's Name :")
+
+#Generating Friendship_percentage With Random Number
+Friendship_percentage = random.randint(0, 100)
+
+#Printing Friendship Percentage
+print("Friendship Between", x1 ,"and", x2 , "is" ,Friendship_percentage, "%")
+
+# Project By Mubashwir Mahadi
